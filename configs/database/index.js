5a56f9e10695038e674ecca93c1f84fa3b7b59df
r3tnuHq3IhgN <nghiephunter.dev.uet@gmail.com>
2023-02-20 14:08:58 +0700

    update db mongo

--- configs/database/index.js
+++ configs/database/index.js
@@ -6,7 +6,7 @@ dotenv.config();
 const connect = async () => {
     try {
         await mongoose.connect(
-            `mongosh "mongodb+srv://phamgiatam11.hwz0dwx.mongodb.net/myFirstDatabase?authSource=%24external&authMechanism=MONGODB-X509" --apiVersion 1 --tls --tlsCertificateKeyFile ../cert.pem` 
+            `mongodb+srv://admin:Root%40123@cluster0.ke96okb.mongodb.net/admin?authSource=admin&replicaSet=atlas-93v2lp-shard-0&w=majority&readPreference=primary&appname=MongoDB%20Compass&retryWrites=true&ssl=true` 
         );
         console.log("connected to MongoDB successfuly");
     } catch (error) {
